fd857c81d6f49349cf2cbd709a3182f21117c3a0
AdamRival <adam.rival44@gmail.com>
2025-11-19 15:13:59 +0100

    rspec de con

--- app/models/review.rb
+++ app/models/review.rb
@@ -5,5 +5,4 @@ class Review < ApplicationRecord
   validates :rating, presence: true
   validates_inclusion_of :rating, :in => 0..5
   validates :rating, numericality: { only_integer: true }
-
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,15 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating 5 restaurants"
+Restaurant.create!(name: "Hello", address: "Nantes", category: Restaurant::CATEGORIES.sample, phone_number: "0606060606")
+Restaurant.create!(name: "Je suis un restaurant", address: "Nantes aussi", category: Restaurant::CATEGORIES.sample, phone_number: "0665352621")
+Restaurant.create!(name: "Chaise", address: "Paris", category: Restaurant::CATEGORIES.sample, phone_number: "0606060606")
+Restaurant.create!(name: "J'ai vraiment pas d'idées", address: "Montpellier", category: Restaurant::CATEGORIES.sample, phone_number: "0699974901")
+Restaurant.create!(name: "Le Lion et L'agneau", address: "Nantes", category: Restaurant::CATEGORIES.sample, phone_number: "0655685420")
+
+
+puts "Finished! Created #{Restaurant.count} restaurants."
